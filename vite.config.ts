88fc3404bd3a893431b611f8cba63da1ede5f35d
Christophe Rosset <tophe@topheman.com>
2026-02-15 11:44:37 +0100

    feat: add server.allowedHost to vite.config.ts .ngrok-free.app
    On iOs, the WebContainer used features that need secure location.
    You can't run with a url like http://192.168.1.13:5173.
    
    Run `ngrok http 5173` or `ngrok http 4173`

--- vite.config.ts
+++ vite.config.ts
@@ -69,6 +69,7 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
     },
+    allowedHosts: [".ngrok-free.app"],
   },
   resolve: {
     alias: {
